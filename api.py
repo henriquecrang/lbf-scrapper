544a557057a907fb696deaae57cd38e819f5fe3e
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-02-27 20:58:17 -0300

    Create batch download

--- api.py
+++ api.py
@@ -1,8 +1,21 @@
 import requests
 
+URL = 'http://lbf.com.br/ws/tempo_real_bybr/json/{id}_tempo_real.json'
+
 
 def client(match_id):
-    url = f'http://lbf.com.br/ws/tempo_real_bybr/json/{match_id}'\
-            '_tempo_real.json'
-    resp = requests.get(url)
+    resp = requests.get(URL.format(id=match_id))
     return resp
+
+
+def batch_download(id_range):
+    jsons = []
+    missed = []
+    for id_ in range(id_range):
+        resp = client(match_id=id_)
+        if resp.status_code == 200:
+            jsons.append(resp.json())
+        else:
+            missed.append(id_)
+
+    return jsons, missed
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,16 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 
 import responses
 
-from api import client
+from api import client, batch_download
 
 
 class TestApi(TestCase):
+    url_base = 'http://lbf.com.br/ws/tempo_real_bybr/json/{id}_tempo_real.json'
+
     @responses.activate
     def test_correct_response(self):
-        url = 'http://lbf.com.br/ws/tempo_real_bybr/json/123_tempo_real.json'
+        url = self.url_base.format(id='123')
         responses.add(
                 responses.GET,
                 url,
@@ -21,3 +23,28 @@ class TestApi(TestCase):
         }
 
         self.assertEqual(resp.json(), expected)
+
+    @mock.patch('api.client')
+    def test_download_all_matches(self, _client):
+        resp_mock_1 = mock.MagicMock()
+        resp_mock_2 = mock.MagicMock()
+        resp_mock_3 = mock.MagicMock()
+        resp_mock_1.status_code = 200
+        resp_mock_1.json.return_value = {'id': 0}
+        resp_mock_2.status_code = 404
+        resp_mock_3.status_code = 200
+        resp_mock_3.json.return_value = {'id': 2}
+
+        _client.side_effect = [resp_mock_1, resp_mock_2, resp_mock_3]
+
+        id_range = 3
+        resp, missed = batch_download(id_range=id_range)
+
+        client_calls = [
+                mock.call(match_id=i) for i in range(id_range)
+        ]
+        _client.assert_has_calls(client_calls)
+        self.assertIsInstance(resp, list)
+        self.assertEqual(resp, [{'id': 0}, {'id': 2}])
+
+        self.assertEqual(missed, [1])
